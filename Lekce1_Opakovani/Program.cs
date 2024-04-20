e3dd28a39a8cf07ab7d9c61385a73554c05dcb0b
AlicePom <129623472+AlicePom@users.noreply.github.com>
2024-04-20 14:23:37 +0200

    upravený pozdrav

--- Lekce1_Opakovani/Program.cs
+++ Lekce1_Opakovani/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Vítejte na kurzu C#2!");
         }
     }
 }
