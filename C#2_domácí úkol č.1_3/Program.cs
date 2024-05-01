d67f6a36e026a4f9beb311fb97a92be12d37ad88
AlicePom <129623472+AlicePom@users.noreply.github.com>
2024-05-01 19:31:26 +0200

    Domácí úkol č.1_část 3
    Snad jsem to takto pochopila dobře, že stačí zadat počet šípů přímo do fieldu PocetSipu v rámci třídy: "Vytvořte třídu Lucistnik, ktera bude mit vlastnost PocetSipu a metodu Vystrel. PocetSipu muze byt nastaven treba na 10".
    
    Pokud ne, tak to opravím (vytvořím příslušnou proměnnou pocetSipu atd). Dej mi, prosím, vědět. Děkuji

---
+++ C#2_domácí úkol č.1_3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace C_2_domácí_úkol_č._1_3
+{
+    internal class Program
+    {
+        public class Lucistnik
+        {
+            public int PocetSipu = 10;
+
+            public int Vystrel()
+            {
+                if (PocetSipu > 0)
+                {
+                    PocetSipu -= 1;
+                    Console.WriteLine($"Vždy se strefím přímo doprostřed! Zbývá mi {PocetSipu} šípů");
+
+                }
+                else
+                {
+                    Console.WriteLine("Nemám šípy.");
+                    Environment.Exit(0);
+                }
+
+                return PocetSipu;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //int pocetSipu = 3;
+            Lucistnik lucistnik = new Lucistnik();
+
+            while (lucistnik.PocetSipu >= 0)
+            {
+                lucistnik.Vystrel();
+            }
+        }
+    }
+}
