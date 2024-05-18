41ccab38225db8105dd2761911751a84e81ed73a
AlicePom <129623472+AlicePom@users.noreply.github.com>
2024-05-18 18:38:43 +0200

    Domácí úkol č. 5
    Ahoj, chci se zeptat ohledně zadání níže:
    
    //Obdelniku vytvorte funkci Vypis informace, ktery vypise vysku a sirku.
    //Vytvoř několik instancí třídy Obdelnik (například 2) a vypiš jejich vlastnosti pomoci metody VypisParameteryObdelniku.
    
    Vytvořila jsem pouze funkci VypisInformace, poněvadž nevidím důvod vytvářet další metodu VypisParameteryObdelniku, která by dělala podle mého názoru v podstatě to samé co VypisInformace. Nejde tedy o chybu v zadání?
    Děkuji.
    
    Hezký zbytek dne,
    
    Alice

---
+++ C#2_6 lekce_breakoutroom_Obdelnik/Obdelnik.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_2_6_lekce_breakoutroom_Obdelnik
+{
+    internal class Obdelnik
+    {
+        public int Sirka;
+        public int Vyska;
+
+        public Obdelnik(int sirka, int vyska)
+        {
+            Sirka = sirka;
+            Vyska = vyska;
+        }
+
+        public Obdelnik(int sirka) : this(sirka, sirka)
+        {
+        }
+
+        public void VypisInformace()
+        {
+            if (Sirka == Vyska)
+            {
+                Console.WriteLine($"Čtverec - délka strany: {Sirka} cm.");
+            }
+            else
+            {
+                Console.WriteLine($"Obdélník - výška: {Vyska} cm, šířka: {Sirka} cm.");
+            }
+        }
+    }
+}
---
+++ C#2_6 lekce_breakoutroom_Obdelnik/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace C_2_6_lekce_breakoutroom_Obdelnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Obdelnik obdelnik = new Obdelnik(4, 7);
+            obdelnik.VypisInformace();
+
+            Obdelnik ctverec = new Obdelnik(8);
+            ctverec.VypisInformace();
+        }
+    }
+}
